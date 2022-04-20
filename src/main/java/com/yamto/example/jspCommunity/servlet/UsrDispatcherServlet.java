10a440c1640c4dfbec2f36a15d9a93f919147dd8
tmedcount <tmedcount@gmail.com>
2022-04-20 16:51:59 +0900

    각 서블릿의 중복되는 코드를 템플릿메서드 패턴으로 중복 제거

--- src/main/java/com/yamto/example/jspCommunity/servlet/UsrDispatcherServlet.java
+++ src/main/java/com/yamto/example/jspCommunity/servlet/UsrDispatcherServlet.java
@@ -1,82 +1,50 @@
 package com.yamto.example.jspCommunity.servlet;
 
-import java.io.IOException;
-
-import javax.servlet.RequestDispatcher;
-import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import com.yamto.example.jspCommunity.container.Container;
 import com.yamto.example.jspCommunity.controller.UsrArticleController;
 import com.yamto.example.jspCommunity.controller.UsrMemberController;
-import com.yamto.example.mysqlutil.MysqlUtil;
 
-// usr/article/list
-// usr/article/detail
-// usr/member/list
+
 @WebServlet("/usr/*")
-public class UsrDispatcherServlet extends HttpServlet {
+public class UsrDispatcherServlet extends DispatcherServlet {
 	@Override
-	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		req.setCharacterEncoding("UTF-8");
-		resp.setContentType("text/html; charset=UTF-8");
-		
-		String requestUri = req.getRequestURI();		
-		String[] requestUriBits = requestUri.split("/");
-		
-		if(requestUriBits.length < 5) {
-			resp.getWriter().append("올바른 요청이 아닙니다.");
-			return;
-		}
-	
-		String controllerName = requestUriBits[3];
-		String actionMethodName = requestUriBits[4];
-		
-		MysqlUtil.setDBInfo("127.0.0.1", "sbsst", "sbs123414", "jspCommunity");
-		
+	protected String doAction(HttpServletRequest req, HttpServletResponse resp, String controllerName, String actionMethodName) {
 		String jspPath = null;
-		
-		if(controllerName.equals("member")) {
+
+		if (controllerName.equals("member")) {
 			UsrMemberController memberController = Container.memberController;
-			
-			if(actionMethodName.equals("list")) {
+
+			if (actionMethodName.equals("list")) {
 				jspPath = memberController.showList(req, resp);
-			} else if(actionMethodName.equals("join")) {
+			} else if (actionMethodName.equals("join")) {
 				jspPath = memberController.showJoin(req, resp);
-			} else if(actionMethodName.equals("doJoin")) {
+			} else if (actionMethodName.equals("doJoin")) {
 				jspPath = memberController.doJoin(req, resp);
 			}
-		} else if(controllerName.equals("article")) {
+		} else if (controllerName.equals("article")) {
 			UsrArticleController articleController = Container.articleController;
-			
-			if(actionMethodName.equals("list")) {
+
+			if (actionMethodName.equals("list")) {
 				jspPath = articleController.showList(req, resp);
-			} else if(actionMethodName.equals("detail")) {
+			} else if (actionMethodName.equals("detail")) {
 				jspPath = articleController.showDetail(req, resp);
-			} else if(actionMethodName.equals("write")) {
+			} else if (actionMethodName.equals("write")) {
 				jspPath = articleController.showWrite(req, resp);
-			} else if(actionMethodName.equals("doWrite")) {
+			} else if (actionMethodName.equals("doWrite")) {
 				jspPath = articleController.doWrite(req, resp);
-			} else if(actionMethodName.equals("modify")) {
+			} else if (actionMethodName.equals("modify")) {
 				jspPath = articleController.showModify(req, resp);
-			} else if(actionMethodName.equals("doModify")) {
+			} else if (actionMethodName.equals("doModify")) {
 				jspPath = articleController.doModify(req, resp);
-			} else if(actionMethodName.equals("doDelete")) {
+			} else if (actionMethodName.equals("doDelete")) {
 				jspPath = articleController.doDelete(req, resp);
 			}
 		}
-				
-		MysqlUtil.closeConnection();
 		
-		RequestDispatcher rd = req.getRequestDispatcher("/jsp/" + jspPath + ".jsp");
-		rd.forward(req, resp);
-	}
-	
-	@Override
-	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		doGet(req, resp);
+		return jspPath;
 	}
 }
